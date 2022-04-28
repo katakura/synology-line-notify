d92d3cb8b7c7f7b0ae4845c857d5f3d011e2a5f4
Yoshimasa Katakura <yoshimasa.katakura@outlook.com>
2022-04-28 18:44:44 +0900

    change null message logic

--- notify.go
+++ notify.go
@@ -15,43 +15,42 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 	// get message parameter
 	msg := r.FormValue("msg")
-	if msg == "" {
-		msg = "no msg"
-	}
 
-	// get line token
-	line_token := r.FormValue("token") // first try from parameter
-	if line_token == "" {
-		line_token = os.Getenv("LINE_TOKEN") // second try from environment parameter
+	if msg != "" {
+		// get line token
+		line_token := r.FormValue("token") // first try from parameter
 		if line_token == "" {
-			fmt.Printf("can't get token\n")
+			line_token = os.Getenv("LINE_TOKEN") // second try from environment parameter
+			if line_token == "" {
+				fmt.Printf("can't get token\n")
+			}
 		}
-	}
 
-	URL := "https://notify-api.line.me/api/notify"
-	u, err := url.ParseRequestURI(URL)
-	if err != nil {
-		fmt.Printf("url parameter error\n")
-	}
+		URL := "https://notify-api.line.me/api/notify"
+		u, err := url.ParseRequestURI(URL)
+		if err != nil {
+			fmt.Printf("url parameter error\n")
+		}
 
-	client := &http.Client{}
+		client := &http.Client{}
 
-	form := url.Values{}
-	form.Add("message", msg)
+		form := url.Values{}
+		form.Add("message", msg)
 
-	body := strings.NewReader(form.Encode())
+		body := strings.NewReader(form.Encode())
 
-	req, err := http.NewRequest("POST", u.String(), body)
-	if err != nil {
-		fmt.Printf("request error\n")
-	}
+		req, err := http.NewRequest("POST", u.String(), body)
+		if err != nil {
+			fmt.Printf("request error\n")
+		}
 
-	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-	req.Header.Set("Authorization", "Bearer "+line_token)
+		req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+		req.Header.Set("Authorization", "Bearer "+line_token)
 
-	_, err = client.Do(req)
-	if err != nil {
-		fmt.Printf("request error\n")
+		_, err = client.Do(req)
+		if err != nil {
+			fmt.Printf("request error\n")
+		}
 	}
 }
 
